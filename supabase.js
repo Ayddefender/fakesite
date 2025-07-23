2fb634cfc87c58a0e0d65bbab734d2848ef84022
Ayddefender <danielolaniyi400@gmail.com>
2025-07-23 18:01:22 +0100

    Create supabase.js

---
+++ supabase.js
@@ -0,0 +1,6 @@
+import { createClient } from '@supabase/supabase-js'
+
+const supabaseUrl = 'https://hniutlajwacxnoqblbyv.supabase.co'
+const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImhuaXV0bGFqd2FjeG5vcWJsYnl2Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTMyNjUyMTQsImV4cCI6MjA2ODg0MTIxNH0.YXx3ZKKHVfw2Tj2ipxrjBCDs8dPJuTEdYtWyn6GznD4'
+
+export const supabase = createClient(supabaseUrl, supabaseAnonKey)
